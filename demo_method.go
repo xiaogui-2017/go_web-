6c187dd9c184f0ee582c3027a774bea0096de258
zzy <923445457@qq.com>
2018-04-08 00:52:52 +0800

    方法使用实例

--- demo_method.go
+++ demo_method.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+// 函数变成方法,方法可以方位接受者(结构体)的字段,就像结构体里访问字段一样
+// area方法属于r和c
+// method可以定义在任何自定义的类型、内置类型、结构体等等
+
+// 可根据自己的需求使用值传递或者指针传递
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+// 可根据自己的需求使用值传递或者指针传递
+func (c Circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
+
+// TODO
+func main() {
+	r1 := Rectangle{12, 2}
+	r2 := Rectangle{9, 4}
+	c1 := Circle{10}
+	c2 := Circle{25}
+	fmt.Println("Area of r1 is: ", r1.area())
+	fmt.Println("Area of r2 is: ", r2.area())
+	fmt.Println("Area of c1 is: ", c1.area())
+	fmt.Println("Area of c2 is: ", c2.area())
+}
--- demo_struct.go
+++ demo_struct.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+type Student struct {
+	Human //匿名字段，默认Student包含Human的所有字段，相当于继承
+	specially string
+}
+
+func main() {
+	// 初始化一个学生
+	mark := Student{Human{"Mark", 25, 120}, "Computer Science"}
+	//查询
+	fmt.Println("His name is", mark.name)
+	fmt.Println("His age is", mark.age)
+	fmt.Println("His anme weight", mark.weight)
+	fmt.Println("His anme specially", mark.specially)
+	//修改
+	fmt.Println("_____________________________________")
+	mark.specially = "AI"
+	fmt.Println("After His anme specially", mark.specially)
+	mark.age = 38
+	fmt.Println("After His age is", mark.age)
+	mark.weight += 60
+	fmt.Println("After His anme weight", mark.weight)
+}
