eef81a9d9331430d830cec9b0d226184c2612a98
xiaogui-2017 <user.email>
2018-04-08 15:29:40 +0800

    方法继承

---
+++ inherit_method.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name  string
+	age   int
+	phone string
+}
+
+type Student struct {
+	Human // 匿名函数
+	school string
+}
+
+type Employee struct {
+	Human // 匿名函数
+	company string
+}
+
+//在human上面定义了一个method
+func (h *Human) SayHi() {
+	//此处call为打电话， 而非调用
+	fmt.Printf("Hi I'm %s, you can all me on %s \n", h.name, h.phone)
+}
+
+func main() {
+	mark := Student{Human{"Mark", 25, "111-111-111"}, "NWNU"}
+	sam := Student{Human{"Sam", 45, "222-222-222"}, "Golang Inc"}
+	mark.SayHi()
+	sam.SayHi()
+}
