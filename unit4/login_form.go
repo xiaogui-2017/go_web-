83ee2c41ed205b63db778f7b0bfdbb2fa97556d6
zhang_zhiyuan <923445457@qq.com>
2018-04-09 16:52:35 +0800

    表单登陆“

--- unit4/login_form.go
+++ unit4/login_form.go
@@ -1,1 +1,40 @@
-package unit4
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"html/template"
+	"log"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // 解析url传递的参数，对于POST则解析响应包的主题(request body)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("Scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Fprintf(w, strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "hello astaxie!")
+}
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		//请求的是登陆数据， 那么执行的登陆的逻辑判断
+		fmt.Println("username: ", r.Form["username"])
+		fmt.Println("password: ", r.Form["password"])
+	}
+}
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
