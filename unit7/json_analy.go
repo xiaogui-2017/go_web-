797081929d00a8af98d139095098e3aa038285cd
xiaogui-2017 <923445457@qq.com>
2018-04-12 10:19:31 +0800

    生成json

--- unit7/json_analy.go
+++ unit7/json_analy.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Server struct {
+	Servername string
+	ServerIP   string
+}
+
+type Serverslice struct {
+	Servers []Server
+}
+
+
+func main() {
+	var s Serverslice
+	str := `{"servers":[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
+	json.Unmarshal([]byte(str), &s)
+	fmt.Println(s)
+}
