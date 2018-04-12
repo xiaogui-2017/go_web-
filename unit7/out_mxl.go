1eceda71146ef5fdc70185aa6dc9cf955fbe5425
xiaogui-2017 <923445457@qq.com>
2018-04-12 10:22:33 +0800

    生产xml

--- unit7/out_mxl.go
+++ unit7/out_mxl.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Servers struct {
+	XMLName xml.Name `xml："servers"`
+	Version string   `xml:"version, attr"`
+	Svs     []server `xml:"server"`
+}
+
+type server struct {
+	ServerName string `xml:"serverName"`
+	ServerIP   string `xml:"serverIP"`
+}
+
+
+func main() {
+	v := &Servers{Version: "1"}
+	v.Svs = append(v.Svs, server{"Shanghai_VPN", "127.0.0.1"})
+	v.Svs = append(v.Svs, server{"Beijing_VPN", "127.0.0.2"})
+	output, err := xml.MarshalIndent(v, " ", " ")
+	if err != nil {
+		fmt.Printf("error:%v \n", err)
+	}
+	os.Stdout.Write([]byte(xml.Header))  // 带xml头，为了生成正确的xml文件
+	os.Stdout.Write(output)
+}
